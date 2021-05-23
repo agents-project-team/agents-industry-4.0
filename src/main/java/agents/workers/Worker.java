99afce0aa9a9d66fc5c64933a02adbd4085da01e
Kiryl Volkau <volkaukiryl@gmail.com>
2021-05-23 14:55:19 +0200

    Add diying behaviour.

--- src/main/java/agents/workers/Worker.java
+++ src/main/java/agents/workers/Worker.java
@@ -1,5 +1,41 @@
 package agents.workers;
 
-public interface Worker {
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
+public abstract class Worker extends Agent {
+    protected volatile boolean isFailing = false;
+    protected AID managerId;
+
+    public Worker(AID managerId) {
+        this.managerId = managerId;
+    }
+
+    @Override
+    protected void setup() {
+        setShuttingDownBehaviour();
+    }
+
+    protected void setShuttingDownBehaviour() {
+        var shuttingDownBehaviour = new CyclicBehaviour() {
+            @Override
+            public void action() {
+//                while (!isFailing) {
+//                    Thread.onSpinWait();
+//                }
+
+                block(3000);
+
+                var iAmDeadMessage = new ACLMessage();
+                iAmDeadMessage.addReceiver(managerId);
+                iAmDeadMessage.setContent("I am dead");
+                iAmDeadMessage.setPerformative(ACLMessage.CANCEL);
+                send(iAmDeadMessage);
+            }
+        };
+
+        this.addBehaviour(shuttingDownBehaviour);
+    }
 }
--- src/main/java/agents/workers/machines/MachineAgent.java
+++ src/main/java/agents/workers/machines/MachineAgent.java
@@ -2,19 +2,23 @@ package agents.workers.machines;
 
 import agents.workers.Worker;
 import jade.core.AID;
-import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
 
-public class MachineAgent extends Agent implements Worker {
+public class MachineAgent extends Worker {
     private String customState;
     private Object bluePrint;
     private int numberOfDetails;
     private AID assemblerId;
     private int seconds;
 
+    public MachineAgent(AID managerId) {
+        super(managerId);
+    }
+
     @Override
     protected void setup() {
+        super.setup();
         Behaviour onePartBehaviour = new CyclicBehaviour() {
             @Override
             public void action() {
